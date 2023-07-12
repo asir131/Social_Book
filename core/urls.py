904eda978e4a61b6af53a79add56be7240a26f9e
Asir Faysal <afaysal220@gmail.com>
2023-07-12 19:58:09 +0600

    First

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('signup',views.signup,name='signup'),
+    path('signin',views.signin,name='signin'),
+    path('logout', views.logoutUser,name='logout'),
+    path('settings', views.settings,name='settings'),
+    path('upload', views.upload,name='upload'),
+    path('like-post', views.like_post,name='like-post'),
+    path('profile/<str:pk>', views.profile,name='profile'),
+    path('follow', views.follow,name='follow'),
+    path('search', views.search,name='search'),
+]
+
